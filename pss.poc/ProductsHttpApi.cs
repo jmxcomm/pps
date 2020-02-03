bf494955b354d5c3841fe646b05f3f70915fe166
Jeroen Matthijssens <jeroen.matthijssens@xcomm.be>
2020-02-03 20:00:04 +0100

    [*] Add publishing message to broker.

--- pss.poc/ProductsHttpApi.cs
+++ pss.poc/ProductsHttpApi.cs
@@ -1,12 +1,14 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -15,6 +17,10 @@ namespace pss.poc
 {
 	public static class ProductsHttpApi
 	{
+		private static readonly string busConnectionString = Environment.GetEnvironmentVariable("queueConnection");
+		private static readonly string busQueueName = Environment.GetEnvironmentVariable("queueName");
+		private static readonly QueueClient broker = new QueueClient(busConnectionString, busQueueName);
+
 		private static readonly string storageConnectionString = Environment.GetEnvironmentVariable("storageConnection");
 		private static readonly string storageContainerName = Environment.GetEnvironmentVariable("containerName");
 		private static readonly BlobServiceClient storage = new BlobServiceClient(storageConnectionString); 
@@ -33,13 +39,7 @@ namespace pss.poc
 			catch (Exception e)
 			{
 				log.LogCritical($"", e);
-				var error = new JObject
-				{
-					["status"] = 400,
-					["title"] = "Failed to fetch state machine",
-					["detail"] = e.Message
-				};
-				return new BadRequestObjectResult(JsonConvert.SerializeObject(error));
+				return Error(400, "Failed to fetch all products", e);
 			}
 		}
 
@@ -61,16 +61,48 @@ namespace pss.poc
 			}
 			catch (Exception e)
 			{
-				log.LogCritical($"Failed uploading blob: {e.Message}", e);
-				var error = new JObject
-				{
-					["status"] = 400,
-					["title"] = "Failed to fetch state machine",
-					["detail"] = e.Message
-				};
-				await Console.Error.WriteLineAsync(e.ToString());
-				return new BadRequestObjectResult(JsonConvert.SerializeObject(error));
+				log.LogCritical($"{e.Message}", e);
+				return Error(400, "Failed to fetch product by id", e);
 			}
+		}
+
+		[FunctionName("ORDER_CREATE")]
+		public static async Task<IActionResult> putOrder(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "orders")]
+			HttpRequest req, ILogger log)
+		{
+			try
+			{
+				JObject payload = JsonConvert.DeserializeObject<JObject>(await new StreamReader(req.Body).ReadToEndAsync());
+
+				BlobClient blob = container.GetBlobClient($"{payload["ExternalId"]}.json");
+				await blob.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload))));
+
+				var messageBody = new JObject();
+				messageBody["ExternalId"] = payload["ExternalId"];
+				var messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageBody));
+				await broker.SendAsync(new Message(messageBytes));
+
+				var responseBody = new JObject();
+				responseBody["status"] = "success";
+				return new OkObjectResult(JsonConvert.SerializeObject (responseBody));
+			}
+			catch (Exception e)
+			{
+				log.LogCritical($"{e.Message}", e);
+				return Error(400, "Failed to create the order", e);
+			}
+		}
+
+		public static IActionResult Error(int status, String title, Exception e)
+		{
+			var error = new JObject
+			{
+				["status"] = 400,
+				["title"] = "Failed to fetch state machine",
+				["detail"] = e.Message
+			};
+			return new BadRequestObjectResult(JsonConvert.SerializeObject(error));
 		}
 	}
 }
